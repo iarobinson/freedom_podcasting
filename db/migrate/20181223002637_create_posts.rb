477260d287ca5f0d9a257e0ff1d0e964b46179b4
iarobinson <ian@freedompodcasting.com>
2018-12-29 11:50:54 -0800

    -Generate Post and Category models. -Create associations between User, Post and Category Models. - Create categories and posts database tables.

--- db/migrate/20181223002637_create_posts.rb
+++ db/migrate/20181223002637_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.references :user, foreign_key: true
-      t.text :body
       t.string :title
-
+      t.text :content
+      t.belongs_to :category, index: true
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_013934) do
+ActiveRecord::Schema.define(version: 2018_12_29_185838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string "email", default: "", null: false
