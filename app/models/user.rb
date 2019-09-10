0572826e0ec10160adb1422d5305c07afcfeec45
Ian Robinson <ian@ianrobinson.net>
2019-09-09 17:41:42 -0700

    Remove :trackable from the user model as I think it's causing our errors in production server

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniautha
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :trackable
+  :recoverable, :rememberable, :validatable
 
   has_many :shows
   has_one_attached :avatar
