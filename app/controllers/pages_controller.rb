b7aed6435d7e87a435a7f68f94a195b1efbf66b8
Ian Robinson <ian@ianrobinson.net>
2019-09-19 16:28:56 -0700

    Update the home controller to differentiate user roles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
 
   def home
+    if current_user.nil?
+      #something...
+    elsif current_user.administrator?
+      @users = User.all
+    end
   end
 end
