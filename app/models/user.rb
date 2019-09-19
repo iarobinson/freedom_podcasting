257f414a02c509f6361bff3c81a0138976eaf7a8
Ian Robinson <ian@ianrobinson.net>
2019-09-19 16:05:39 -0700

    Implement user roles and dashboards based on the role of the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniautha
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+  # Set roles and assign new people to stranger roles unless specified
+  after_initialize :set_default_role, :if => :new_record?
+  enum role: [:administrator, :producer, :client, :stranger]
+  def set_default_role
+    self.role ||= :stranger
+  end
 
   has_and_belongs_to_many :shows
   has_one_attached :avatar
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_212223) do
+ActiveRecord::Schema.define(version: 2019_09_19_223808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_212223) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "status"
+    t.integer "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
