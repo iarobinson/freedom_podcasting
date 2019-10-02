53b630104754c5a648700ab5b380cf8234f52ac7
iarobinson <ian@ianrobinson.net>
2019-10-02 13:57:06 -0700

    Comment out sudo code:

---
+++ app/helpers/episodes_helper.rb
@@ -0,0 +1,22 @@
+module EpisodesHelper
+
+  # def set_episode_production_cost
+  #   if episode.duration <= 10 minutes
+  #     episode.production_cost = 80
+  #   else if episode.duration <= 20 minutes
+  #     episode.production_cost = 100
+  #   else if episode.duration <= 30 minutes
+  #     episode.production_cost = 120
+  #   else if episode.duration <= 40 minutes
+  #     episode.production_cost = 140
+  #   else if episode.duration <= 50 minutes
+  #     episode.production_cost = 160
+  #   else if episode.duration <= 30 minutes
+  #     episode.production_cost = 180
+  #   else if episode.duration <= 30 minutes
+  #     episode.production_cost = 200
+  #   else if episode.duration <= 30 minutes
+  #     episode.production_cost = 220
+  #   end
+  # end
+end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,8 @@
 class Show < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :episodes
+  has_one :feed
 
   has_one_attached :show_art
+
 end
