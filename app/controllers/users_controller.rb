e36f7043dce1703195e1a1b53e94c16b077b53ff
Ian Robinson <ian@ianrobinson.net>
2019-11-02 14:56:05 -0700

    Add explanation to tools with a screenshot Update feed flipper tool to describe to new users what it does Update user controller to restrict unauthenticated access to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!
 
   def index
     @users = User.all
