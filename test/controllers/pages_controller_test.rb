9f0f31b0125695f515bcbbd25f136fba3cae9594
Ian Robinson <ian@ianrobinson.net>
2020-02-10 15:05:29 +0700

    Testing Semaphor... even though my tests are broken, semaphor SHOULD show tests passing

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,6 +6,5 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_match(/[a-zA-Z]/,"kaboom", "MEssage here")
     assert_match(//, "asadfasdfasdf.")
-    assert(1 == 2)
   end
 end
