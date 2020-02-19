cec0875e894bce3c675eedb7322abea467d3b634
Ian Robinson <ian@ianrobinson.net>
2020-02-19 16:54:19 +0700

    Upgrade to ruby 2.6.5

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require 'capybara/rails'
 require 'capybara/rspec'
-p "Test helper loaded"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
