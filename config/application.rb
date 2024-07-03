3e2d850dbd691762752ca7685df8f9d5975c75e4
useful_programmer <ian@ianrobinson.net>
2024-07-03 15:19:22 -0700

    Upgrade application version in application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Freedompodcasting
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults "6.0"
+    config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
