a8b25f6d8fe7d79e5341d080716feac163d08b2a
useful_programmer <ian@ianrobinson.net>
2024-07-10 10:06:12 -0700

    Making button styling consistent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def the_15th_of_last_month
     Date.new(Time.now.year, Time.now.month, 15).prev_month
   end
+  def common_big_bold_button
+    "w-100 btn btn-lg btn-primary my-2"
+  end
 end
