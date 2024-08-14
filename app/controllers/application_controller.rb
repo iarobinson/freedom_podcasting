e842ff6e25190d9507d361a4daeb4b708db575cb
useful_programmer <ian@ianrobinson.net>
2024-08-14 13:14:27 -0700

    Remove CORS nonsense from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :allow_cross_domain_access
-  after_action :cors_set_access_control_headers
-
-  def allow_cross_domain_access
-      headers['Access-Control-Allow-Origin'] = '*'# http://localhost:9000
-      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
-      headers['Access-Control-Max-Age'] = '1728000'
-  end
-  def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
-      headers['Access-Control-Max-Age'] = "1728000"
-  end
 end
