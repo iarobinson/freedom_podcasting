9ba0dfe779535ada3e2e6e349ad9bf075d06ea20
useful_programmer <ian@ianrobinson.net>
2024-09-09 20:18:22 -0700

    Adjust views to optimize for those who want to upload and get media urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     Date.new(Time.now.year, Time.now.month, 15).prev_month
   end
 
-  def common_big_bold_button style = 'primary'
+  def common_big_bold_button(style='primary')
     "w-100 btn btn-lg btn-#{style} my-2"
   end
 end
