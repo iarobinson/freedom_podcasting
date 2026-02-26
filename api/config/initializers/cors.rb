187a6b4aac9d8807aca477580dde7f302989a973
Ian <ian@ianrobinson.net>
2026-02-26 13:55:40 -0800

    fix: allow fly.dev origins in CORS

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -4,9 +4,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       "http://localhost:3001",
       "http://localhost:3000",
       /https:\/\/.*\.freedompodcasting\.com/,
-      ENV.fetch("FRONTEND_URL", "")
+      /https:\/\/.*\.fly\.dev/
     )
-
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
